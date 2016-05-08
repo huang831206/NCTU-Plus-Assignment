633e3f23ab2457a541b4b9afd87997e521923761
Liang-Chi Tseng <lctseng@cs.nctu.edu.tw>
2016-05-08 16:23:48 +0800

    fix redis.current , using url

--- config/initializers/redis_init.rb
+++ config/initializers/redis_init.rb
@@ -1,6 +1,5 @@
 if ENV["HEROKU_REDIS_SILVER_URL"]
-  host = URI.parse(ENV["HEROKU_REDIS_SILVER_URL"]).host
-  REDIS = Redis.new(:host => host)
+  REDIS = Redis.new(:url => ENV["HEROKU_REDIS_SILVER_URL"])
 else
   REDIS = Redis.current
 end

85ad70bf040353f076c5085501077f1141eb1ac2
Liang-Chi Tseng <lctseng@cs.nctu.edu.tw>
2016-05-09 16:20:19 +0800

    add tz to Taipei

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module NctuPlusTest
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.time_zone = "Taipei"
     config.active_record.default_timezone = :local
   end
 end
